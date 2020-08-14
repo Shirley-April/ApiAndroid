7021abe5ec0470d0d2fbe92174ec79e1f243964d
Shirley-April <shirleyapril23@gmail.com>
2020-08-14 22:06:13 +0300

    first-commit

---
+++ app/src/main/java/com/example/myhello/RegistrationResponse.kt
@@ -0,0 +1,8 @@
+package com.example.myhello
+
+import com.google.gson.annotations.SerializedName
+
+data class RegistrationResponse(
+    @SerializedName("message") var message: String,
+    @SerializedName("student") var student: Student
+)
